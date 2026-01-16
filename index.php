a0e8e44f1d9a1f980681fb7f40ec75529d4680bb
aminotsukasa(▼´･ω･`▼) <aminotsukasa@users.noreply.github.com>
2026-01-16 11:57:16 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,11 @@
 echo date('r');
 #phpinfo();
 #<span style="color: blue;">
+
+$host = 'localhost';
+$dbname = 'my_database';
+$username = 'db_user';
+$password = 'super_secret_password123!'; // sample
+$pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
 ?>
